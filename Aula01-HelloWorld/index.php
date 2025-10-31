27f949bf70feb100f3dbec5b27ec16f9ee52c40b
Gotenks0853V <ricardi.gt.pvh@gmail.com>
2025-10-31 12:48:21 -0400

    Concatenação de duas variáveis utilizando o ponto (.)

--- Aula01-HelloWorld/index.php
+++ Aula01-HelloWorld/index.php
@@ -6,4 +6,7 @@
 
     // Concatenação simples de strings, feita com aspas duplas
     echo "Hello World! $primeiroNome $segundoNome";
+
+    // Concatenação utilizando ponto(.) para juntar variáveis
+    echo '<br> Hello World! '.$primeiroNome.' '.$segundoNome;
 ?>
