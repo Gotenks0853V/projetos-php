d3d93bafa32d91dcc3b628e28e3d6af20b37acaf
Gotenks0853V <ricardi.gt.pvh@gmail.com>
2025-10-31 13:13:47 -0400

    Utilizando string de difetentes formas e adicionando um contador de caracteres

---
+++ Aula02-TiposVariaveis/index.php
@@ -0,0 +1,26 @@
+<?php
+
+    //Exemplo de string:
+    $nome = 'Alessandro Kobs';
+    $numeroCaracteres = strlen($nome);
+
+    // Atalho Ctrl+/ para comentar linhas selecionadas
+    // Exibir:
+    // echo $nome;
+
+    // Concatenar
+    // Echo $nome.$nome;
+
+    // Exibir junto com string:
+    //echo "Meu nome é: $nome";
+
+    // Contar caracteres (strlen)
+    //echo strlen($nome);
+
+    echo $numeroCaracteres;
+
+    //Exemplo de integer:
+    //Exemplo de float (decimal):
+    //Exemplo de boolean:
+
+?>
