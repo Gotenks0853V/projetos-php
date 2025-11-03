5f063e62f15ff1e43497bbc5e0c652ecdf20c317
Gotenks0853V <ricardi.gt.pvh@gmail.com>
2025-11-03 10:18:32 -0400

    Manipulação de dados com funções do array

--- Aula03-Arrays/index.php
+++ Aula03-Arrays/index.php
@@ -1,5 +1,5 @@
 <?php
-
+/*  Aula introdução
     $nomes = array("Alessandro", "João");
     //echo $nomes[1];
     $numeros = ["1", "2", "30"];
@@ -29,9 +29,42 @@
             "cidade" => "São Paulo"
         ]
     ];
-
+    // Operação de debug usavada para imprimir o array na tela.
     echo "<pre>";
     var_dump($informacoes);
+    echo "</pre>";
+    */
+
+    $frutas = ["Laranja", "Limão"];
+    $outras_frutas = ["Abacaxi", "Uva"];
+
+    // echo "<pre>";
+    // var_dump($frutas);
+    // echo "</pre>";
+
+    //array_push($frutas, "Uva");
+    //Remove último item de um array
+    //array_pop($frutas);
+
+    //Remove primeiro item de um array
+    //array_shift($frutas);
+
+    // Adicionar um novo indice
+    $frutas[2] = "Morango";
+
+    // Modificar um indice existente
+    //$frutas[0] = "Uva";
+
+    //$quantidadeDeItems = count($frutas);
+    //echo $quantidadeDeItems;
+
+    $temAbacaxi = in_array("Abacaxi", $frutas);
+    echo $temAbacaxi;
+
+    $todasFrutas = array_merge($frutas, $outras_frutas);
+
+    echo "<pre>";
+    var_dump($todasFrutas);
     echo "</pre>";
 
 ?>
