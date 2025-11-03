84a2c09eb5a9655541c4e86a71deecef843ae784
Gotenks0853V <ricardi.gt.pvh@gmail.com>
2025-11-03 12:18:41 -0400

    Exercicios com arrays

--- Aula03-Arrays/index.php
+++ Aula03-Arrays/index.php
@@ -35,6 +35,7 @@
     echo "</pre>";
     */
 
+/*
     $frutas = ["Laranja", "Limão"];
     $outras_frutas = ["Abacaxi", "Uva"];
 
@@ -66,5 +67,58 @@
     echo "<pre>";
     var_dump($todasFrutas);
     echo "</pre>";
+    */
+
+/*
+    Exercicio 01:
+    1. Crie um array chamado Cidades contendo o nome de 5 cidades que você quer visitar.
+    2. Imprima na tela o valor da terceira cidade deste array.
+*/
+
+    $cidades = ["Ushuaia", "Luang Prabang", "Bergen", "Essaouira", "Rotorua"];
+    echo $cidades[2];
+
+/*
+    Exercicio 02:
+    1. Crie um array associativo chamado $alunos onde as chaves são os nomes de três alunos e os valores são as idades deles.
+    2. Imprima a idade do segundo aluno.
+*/
+
+    $alunos = [
+        "João" => 14,
+        "Maria" => 16,
+        "josé" => 17
+    ];
+
+    echo $alunos["Maria"];
+
+/*
+    Exercicio 03:
+    1. Crie um array chamado $cores contendo três cores.
+    2. Adicione uma nova cor ao final do array.
+    3. Remova a primeira cor do array.
+    4. Imprima o array resultante.
+*/
+
+    $cores = ["Vermelho", "Verde", "Azul"];
+
+    $cores[] = ["Amarelo"];
+    array_shift($cores);
+
+    var_dump($cores);
+    print_r($cores);
+
+/*
+    Exercicio 04:
+    Crei um array associativo chamado $precos onde as chaves são nomes de produtos e valores são seus preços.
+    Atualize o preço de um dos produtos adicionando + R$ 10,00.
+    Atualize o preço do primeiro produto reduzindo - R$ 1,00.
+    Imprima o array resultante.    
+ */
+
+    $precos = ["Bola" => 10, "Skate" => 250, "Patinete" => 100];
+    $precos["Patinete"] += 10;
+    $precos["Bola"] -= 1;
+    print_r($precos);
 
 ?>
