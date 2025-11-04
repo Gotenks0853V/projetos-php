47a0429dda6062efcc675deddb223b206f94ad25
Gotenks0853V <ricardi.gt.pvh@gmail.com>
2025-11-04 08:48:40 -0400

    Exemplos de operadores lógicos AND, OR e Negação

--- Aula04-Condicionais/index.php
+++ Aula04-Condicionais/index.php
@@ -1,4 +1,5 @@
 <?php
+/*  Parte 1
     //Estruturas Condicionais
     //$nome = 'Alessandro';
 
@@ -18,5 +19,41 @@
         echo 'Você pode acessar o sistema';
     } else {
         echo 'Você não pode acessar o sistema';
+    }
+    */
+
+/*   Parte 2*/
+    // Operadores lógicos
+
+    // &&/and Operador lógico E
+    // Verifica se AMBAS as condiçoes são verdadeiras.
+
+    // $idade = 18;
+    // $temCarteira = false;
+
+    // if ($idade >= 18 && $temCarteira) {
+    //     echo 'Você pode dirigir no Brasil';
+    // } else {
+    //     echo 'Você Não pode dirigir no Brasil';
+    // }
+    
+    // ||/or Operador lógico OU
+    // $idade = 18;
+    // $temCarteira = false;
+
+    // if ($idade >= 18 || $temCarteira) {
+    //     echo 'Você pode dirigir uma nave espacial';
+    // } else {
+    //     echo 'Você Não pode dirigir uma nave espacial';
+    // }
+
+    // /! Operador lógico de negação
+
+    $temCarteira = false;
+
+    if (!$temCarteira) {
+        echo 'Você não pode dirigir';
+    } else {
+        echo 'Você pode dirigir';
     }
 ?>
