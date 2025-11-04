3219640984fa3007222300acffa14528eae91866
Gotenks0853V <ricardi.gt.pvh@gmail.com>
2025-11-04 08:08:01 -0400

    Começando a trabalhar com condicionais

---
+++ Aula04-Condicionais/index.php
@@ -0,0 +1,18 @@
+<?php
+    //Estruturas Condicionais
+    $nome = 'Alessandro';
+
+    // Operadores de Comparação
+
+    // == (igual)
+    // != (Diferente)
+
+    // > (Maior que)
+    // < (Menor que)
+    // >= (Maior ou igual a)
+    // <= (Menor ou igual a)
+
+    if ($nome == 'Alessandro') {
+        echo 'Olá Alessandro';
+    }
+?>
