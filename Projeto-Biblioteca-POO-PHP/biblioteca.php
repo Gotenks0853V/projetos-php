1d43911785faf7b6892238ba848313066a869dbf
Gotenks0853V <ricardi.gt.pvh@gmail.com>
2025-11-06 11:43:41 -0400

    Enviando modelo básico para biblioteca

--- Projeto-Biblioteca-POO-PHP/biblioteca.php
+++ Projeto-Biblioteca-POO-PHP/biblioteca.php
@@ -4,6 +4,90 @@
             public string $titulo, $autor;
             public bool $disponivel;
 
+            function estaDisponivel()
+            {
+
+            }
+
+            function marcarComoEmprestado()
+            {
+
+            }
+
+            function marcarComoDisponivel()
+            {
+                
+            }
+        }
+
+        class Estante {
+            public $livros = [];
+
+            function adicionarLivro()
+            {
+
+            }
+
+            function removerLivro()
+            {
+
+            }
+
+            function buscarLivroPorTitulo()
+            {
+
+            }
+
+            function listarLivrosDisponiveis()
+            {
+
+            }
+        }
+
+        class Usuario {
+            public string $nome;
+            public $livrosEmprestados = [];
+            public $tipo;
+
+            function podePegarEmprestado()
+            {
+
+            }
+
+            function adicionarLivroEmprestado()
+            {
+
+            }
+
+            function removerLivroEmprestado()
+            {
+
+            }
+
+            function listarLivrosEmprestados()
+            {
+
+            }
+        }
+
+        // class Professor {
+
+        // }
+
+        // class Aluno {
+
+        // }
+
+        class Bibliotecario {
+            function emprestarLivro(Usuario $usuario, Livro $livro, Estante $estante)
+            {
+
+            }
+
+            function devolverLivro()
+            {
+
+            }
         }
     }
 ?>
